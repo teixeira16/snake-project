f618f3bc63780a1b8af220d6e56b9101a0f939e2
teixeira16 <pedropradoteixeira@gmail.com>
2017-07-06 21:06:58 +0100

    first commit

---
+++ Game-over.py
@@ -0,0 +1,10 @@
+
+
+message = "Game Over"
+mesage2 = "NOOB!!"
+speed=0.05
+
+sense.show_message(message, speed, text_colour=w, back_colour=e) 
+sense.show_message(message, speed, text_colour=r, back_colour=e) 
+
+
---
+++ README.md
@@ -0,0 +1,3 @@
+Jogo Snake
+Criado por: Mário Francisco Silva (Engatatão Profissional) e Pedro Teixeira
+Obrigado e um resto de um bom dia!
